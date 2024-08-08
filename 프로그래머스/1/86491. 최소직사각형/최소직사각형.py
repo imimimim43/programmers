61e72787c4d148760e966084b95324dd780e74df
imimimim43 <102197100+imimimim43@users.noreply.github.com>
2024-08-08 14:29:59 +0900

    [level 1] Title: 최소직사각형, Time: 5.56 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -2,15 +2,13 @@ from collections import deque
 
 def solution(sizes):
     answer = 0
-    list_a = []
-    list_b = []
+    row = 0
+    col = 0
     
-    for i in range(len(sizes)):
-        if sizes[i][0] < sizes[i][1]:
-            list_a.append(sizes[i][1])
-            list_b.append(sizes[i][0])
-        else:
-            list_a.append(sizes[i][0])
-            list_b.append(sizes[i][1])
-            
-    return max(list_a)*max(list_b)
+    for i, j in sizes:
+        if i < j:
+            i, j = j, i
+        row = max(row, i)
+        col = max(col, j)
+    
+    return row*col
