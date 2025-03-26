46b924bcea9f31949a368fa43ada9af8ddf276a7
Eunji IM <102197100+imimimim43@users.noreply.github.com>
2025-03-26 11:17:59 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,8 @@
+function solution(num_list, n) {
+    var answer = [[]];
+    
+    for(let i = 0; i<num_list.length/n; i++){
+        answer[i] = num_list.slice(i*n,(n+(i*n)));
+    }
+    return answer;
+}
