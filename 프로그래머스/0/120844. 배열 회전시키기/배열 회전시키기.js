94ff83c998c93b479a6f90419b5bd97591bf4d7b
Eunji IM <102197100+imimimim43@users.noreply.github.com>
2025-03-26 16:08:09 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,11 @@
+function solution(numbers, direction) {
+    if(direction === "right"){
+        let a = numbers.pop();
+        numbers.unshift(a);        
+    }
+    else{
+        let a = numbers.shift();   
+        numbers.push(a);
+    }
+    return numbers;
+}
