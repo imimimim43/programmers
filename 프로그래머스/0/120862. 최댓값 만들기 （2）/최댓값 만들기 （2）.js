b207ca519f1c962773dd22df29641bc04fbb1b71
Eunji IM <102197100+imimimim43@users.noreply.github.com>
2025-04-08 16:35:58 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
     let n = numbers.length-1;
-    console.log(numbers.sort((a,b)=> a-b));
+    numbers.sort((a,b)=> a-b);
     return numbers[0]*numbers[1] > numbers[n]*numbers[n-1]? numbers[0]*numbers[1] : numbers[n]*numbers[n-1];
     
 }
